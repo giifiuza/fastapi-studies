127d1d2851b9f459f0428e4b285369b53d4bcb02
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-08-28 11:04:25 -0300

    upload

--- fastapi-studies/main.py
+++ fastapi-studies/main.py
@@ -1,28 +1,36 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-# class Item(BaseModel):
-#     name: str
-#     description: str 
-#     price: float
-#     tax: float 
+class Curso(BaseModel):
+    id: Optional[int]
+    name: str
+    aulas: float 
+    horas: float
+    instrutor: str 
 
 cursos = {
     1: {
-        "nome": "python",
+        "name": "python",
         "aulas": 20,
         "horas": 80,
         "instrutor": "Cleber"
     },
     2: {
-        "nome": "Java",
+        "name": "Java",
         "aulas": 15,
         "horas": 60,
         "instrutor": "Leonardo"
     },
 }
+
+
+@app.get("/")
+async def get_all_cursos():
+    return cursos
+
 
 @app.get("/cursos/{curso_id}")
 async def get_cursos(curso_id: int):
@@ -32,6 +40,12 @@ async def get_cursos(curso_id: int):
         return {"message": curso}
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!')
+
+@app.post("/cursos/adicionar")
+async def post_cursos(curso: Curso):
+    curso.id = len(cursos) + 1
+    cursos[curso.id] = curso
+    return curso
 
 if __name__ == '__main__':
     import uvicorn
