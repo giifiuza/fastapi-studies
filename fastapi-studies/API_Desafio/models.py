fd5cb8d72d65f8761506765241634cdb9f0e297f
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-09-14 11:09:00 -0300

    Finished

--- fastapi-studies/API_Desafio/models.py
+++ fastapi-studies/API_Desafio/models.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class Cat(BaseModel):
-    id: Optional[str] = None
+class Dog(BaseModel):
+    id: Optional[int] = None
     raca: str
     tempo_vida: str
     descricao: str 
